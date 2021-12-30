ae2b6573ecbf7294527ac2ca4189d194bf9cd530
iotronlab <iotronlabspvt@gmail.com>
2021-12-30 16:21:01 +0530

    updated contactus

--- components/contact/Form.vue
+++ components/contact/Form.vue
@@ -1,19 +1,241 @@
 <template>
   <v-container>
-    <v-text-field label="Name" outlined></v-text-field>
-    <v-text-field label="Email" outlined></v-text-field>
-    <v-text-field label="Contact" outlined></v-text-field>
-    <v-text-field label="Location" outlined></v-text-field>
-    <v-text-field label="Business Link" outlined></v-text-field>
-    <v-text-field
-      label="How did you find us? (Referral)"
-      outlined
-    ></v-text-field>
-    <v-textarea label="Describe your Query" outlined></v-textarea>
+    <div v-if="!formSubmitted" class="pa-1">
+      <validation-observer ref="observer">
+        <v-col cols="12" lg="8" class="mx-auto card-glass rounded-xl">
+          <validation-provider
+            v-slot="{ errors }"
+            name="Full Name"
+            rules="required"
+          >
+            <v-text-field
+              v-model="form.name"
+              outlined
+              hint="Enter your full name"
+              label="Full Name"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Email"
+            rules="required|email"
+          >
+            <v-text-field
+              v-model="form.email"
+              outlined
+              hint="Enter your email"
+              label="Email"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+
+          <validation-provider
+            v-slot="{ errors }"
+            name="Mobile Number"
+            rules="required|digits:10"
+          >
+            <v-text-field
+              v-model="form.contact"
+              counter
+              outlined
+              maxlength="10"
+              type="number"
+              prefix="+91"
+              hint="Enter your contact number"
+              label="Mobile Number"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Location"
+            rules="required"
+          >
+            <v-text-field
+              v-model="form.location"
+              outlined
+              hint="Enter Location"
+              label="Location"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Category"
+            rules="required"
+          >
+            <v-select
+              v-model="form.category"
+              :items="categoryOptions"
+              :error-messages="errors"
+              label="Category"
+              outlined
+            ></v-select>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Genres"
+            rules="required"
+          >
+            <v-combobox
+              v-model="form.genres"
+              :items="genreOptions"
+              item-text="name"
+              item-value="name"
+              :search-input.sync="search"
+              hide-selected
+              hint="select or add your own"
+              label="Genres"
+              multiple
+              outlined
+              persistent-hint
+              small-chips
+              :error-messages="errors"
+            >
+              <template #no-data>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      No results for "<strong>{{ search }}</strong
+                      >". Press <kbd>enter</kbd> to create a new one
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-combobox>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Languages"
+            rules="required"
+          >
+            <v-combobox
+              v-model="form.languages"
+              :items="languageOptions"
+              item-text="name"
+              item-value="name"
+              :search-input.sync="search"
+              hide-selected
+              hint="select or add your own"
+              label="Languages"
+              multiple
+              outlined
+              persistent-hint
+              small-chips
+              :error-messages="errors"
+            >
+              <template #no-data>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      No results for "<strong>{{ search }}</strong
+                      >". Press <kbd>enter</kbd> to create a new one
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-combobox></validation-provider
+          >
+          <validation-provider
+            v-slot="{ errors }"
+            name="Video Link"
+            rules="required"
+          >
+            <v-text-field
+              v-model="form.video_link"
+              outlined
+              hint="Enter your Video Link"
+              label="Video Link"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Profile Link"
+            rules="required"
+          >
+            <v-text-field
+              v-model="form.profile_link"
+              outlined
+              hint="Enter your Profile Link"
+              label="Profile Link"
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+
+          <v-btn block color="primary" class="mb-2" @click="submit"
+            >Submit</v-btn
+          >
+          <v-alert v-if="message" type="error">
+            {{ message }}
+          </v-alert>
+        </v-col></validation-observer
+      >
+    </div>
+    <div v-else class="ma-2">
+      <v-alert v-if="message" type="success" outlined>
+        {{ message }}
+      </v-alert>
+    </div>
   </v-container>
 </template>
 
+
 <script>
-export default {}
+export default {
+  data: () => ({
+    form: {
+      email: null,
+      name: null,
+      contact: null,
+      location: null,
+      category: null,
+      genres: null,
+      languages: null,
+      video_link: null,
+      profile_link: null,
+    },
+    genreOptions: [],
+    languageOptions: [],
+    categoryOptions: ['Solo', 'Duo/Trio', 'Band/Musicians'],
+    agreeTerms: false,
+    search: null,
+    message: null,
+    formSubmitted: false,
+  }),
+  async fetch() {
+    await this.$axios
+      .$get('filters')
+      .then((res) => {
+        this.genreOptions = res.data.Genres
+        this.languageOptions = res.data.Languages
+      })
+      .catch((err) => {
+        this.$sentry.captureException(new Error(err))
+      })
+  },
+  head() {
+    return {
+      title: 'Join US',
+    }
+  },
+  methods: {
+    async submit() {
+      if (await this.$refs.observer.validate()) {
+        await this.$axios
+          .$post('forms/artist', this.form)
+          .then((res) => {
+            this.message = res.message
+            this.formSubmitted = true
+          })
+          .catch((err) => {
+            this.message = err.response.data.message
+            this.$sentry.captureException(new Error(err))
+          })
+      }
+    },
+  },
+}
 </script>
 
--- pages/contactus.vue
+++ pages/contactus.vue
@@ -2,21 +2,29 @@
   <v-container>
     <h1 class="text-h2 text-center mb-10">Contact Us</h1>
     <v-row no-gutters justify="center">
-      <v-col cols="12" lg="7">
-        <ContactForm />
-      </v-col>
       <v-col cols="12" lg="4">
         <h2>Get in Touch</h2>
         <v-divider class="mb-4"></v-divider>
         <h2 class="text-body-1">
-          If you are an artist looking to collaborate, click here!
+          If you are an artist looking to collaborate,
+          <NuxtLink to="/joinus"> click here!</NuxtLink>
         </h2>
-        <address>
+        <address class="mb-4">
           Visit us at:<br />
           97, Ajay Nagar, Melabagan Estate,
           <br />Basak Bagan, Kolkata, <br />West Bengal 700074
         </address>
-        <h2 class="text-body-1"></h2>
+
+        <a href="tel:+91 7439 790 770">
+          <v-icon>{{ icons.phone }} </v-icon>&nbsp;+91 7439 790 770
+        </a>
+        <a href="mailto:contactus@iaa.org.in">
+          <v-icon>{{ icons.email }}</v-icon
+          >&nbsp;contactus@iaa.org.in
+        </a>
+      </v-col>
+      <v-col cols="12" lg="7">
+        <ContactForm />
       </v-col> </v-row
   ></v-container>
 </template>
