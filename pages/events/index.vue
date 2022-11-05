413964d6dc0523c2218dbc6f265116a05be24dde
iotronlab <50877415+iotronlab@users.noreply.github.com>
2022-11-05 20:30:57 +0530

    build: event

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container fluid class="pa-0">
-             <!-- <FilterNav /> -->
+  <v-container fluid class="pa-0 glow-purple">
+    <!-- <FilterNav /> -->
     <section v-if="$fetchState.pending">
       <h1 class="text-lg-h1 text-h2 text-center">Events</h1>
       <Loading message="fetching events..." />
@@ -24,27 +24,28 @@
             <h1 class="text-lg-h1 text-h2">Events</h1>
             <v-divider class="my-2"></v-divider>
           </v-col>
-            <h2 class="text-body-1 mb-2">
-          <div class="alignment-box">
+          <h2 class="text-body-1 mb-2">
+            <div class="alignment-box">
               <v-pagination v-model="pageData.current_page" :length="pageData.last_page" :next-icon="nextArrow"
                 :prev-icon="prevArrow" @input="updateQuery(pageData.current_page)"></v-pagination>
-            <FilterNav />
-          </div>
-              showing ({{ pageData.from }} - {{ pageData.to }})
-              <span v-if="pageData.total > 1">events</span>
-              <span v-else>event</span>
-            </h2>
+              <FilterNav />
+            </div>
+            showing ({{ pageData.from }} - {{ pageData.to }})
+            <span v-if="pageData.total > 1">events</span>
+            <span v-else>event</span>
+          </h2>
         </v-row>
 
         <!-- events for loop -->
         <section>
           <v-col cols="12" lg="10" class="mx-auto pa-0 ma-0">
-          <v-row no-gutters justify="center" class="ma-0">
-            <v-col v-for="(event, i) in events" :key="i" cols="12" lg="4" md="5" sm="5" style="z-index: 10" class="pa-0 ma-0 my-4">
-              <EventCardNew :event="event" />
-            </v-col>
-          </v-row>
-        </v-col>
+            <v-row no-gutters justify="center" class="ma-0">
+              <v-col v-for="(event, i) in events" :key="i" cols="12" lg="4" md="5" sm="5" style="z-index: 10"
+                class="pa-0 ma-0 my-4">
+                <EventCardNew :event="event" />
+              </v-col>
+            </v-row>
+          </v-col>
         </section>
 
         <h2 class="text-body-1 text-center mb-6">
@@ -54,7 +55,7 @@
           showing ({{ pageData.from }} - {{ pageData.to }})
           <span v-if="pageData.total > 1">events</span>
           <span v-else>event</span>
- 
+
         </h2>
       </section>
     </section>
