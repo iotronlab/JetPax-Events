5e66711c9b66fc539131d6909bbad427d7d3f393
SykaaInc <viswampc143@gmail.com>
2022-11-12 14:07:09 +0530

    build: gsap-draggable

--- pages/tests/gsap.vue
+++ pages/tests/gsap.vue
@@ -1,60 +1,114 @@
 <template>
-  <div class="stage">
-    <div class="wrapper">
-      <div class="ring">
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
-        <div class="carde"></div>
+
+  <div class="stage" id="stage">
+    <div class="wrapper" id="wrapper">
+      <div class="ring" id="ring">
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <div class="card-item"></div>
+        <!-- div v-for="(event, i) in events" :key="i" class="card-item">
+          <EventCardNew :event="event" />
+        </div -->
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
+import Draggable from "gsap/Draggable"
+
 export default {
-  mounted() {
-    this.init();
+  data() {
+    return {
+      events: [],
+    }
   },
+
+  mounted() {
+    this.$axios
+      .$get(`events`, { params: { page: 1 } })
+      .then((res) => {
+        this.events = res.data
+        this.init()
+      })
+  },
+
   methods: {
     init() {
-      const gsap = this.$gsap
-      const card = document.querySelectorAll('.carde')
-      const ring = document.querySelectorAll('.ring')
 
-      gsap.set(ring, { rotationY: 180, cursor: 'drag' }) // set initial rotationY so the parallax jump happens off screen
-      gsap.set(card, { // apply transform rotations to each image
-        rotateY: (i) => getRotate(i),
-        transformOrigin: '50% 50% 500px',
-        z: -500,
-        backgroundImage: (i) => 'url(https://picsum.photos/id/' + (i + 32) + '/600/400/)',
-        backgroundPosition: (i) => getBgPos(i),
-        backfaceVisibility: 'hidden',
-      })
-      console.log(card, ring)
-      gsap.timeline()
-        .from(card, {
-          duration: 1.5,
-          y: 200,
-          opacity: 0,
-          stagger: 0.1,
-          ease: 'expo'
+      this.$nextTick(() => {
+        this.$nextTick(() => {
+
+          const gsap = this.$gsap
+          gsap.registerPlugin(Draggable)
+
+          let xPos = 0
+          const card = document.querySelectorAll('.card-item')
+
+          const ring = document.getElementById('ring')
+          const wrapper = document.getElementById('wrapper')
+          const mainContent = document.getElementById('stage')
+
+          gsap.set(ring, { rotationY: 180, cursor: 'drag' })
+          gsap.set(card, { 
+            rotateY: (i) => getRotate(i),
+            transformOrigin: '50% 50% 500px',
+            z: -500,
+            backfaceVisibility: 'hidden',
+          })
+
+          gsap.timeline()
+            .from(card, {
+              duration: 1.5,
+              y: 200,
+              opacity: 0,
+              stagger: 0.1,
+              ease: 'expo'
+            })
+
+          Draggable.create(wrapper, {
+            trigger: mainContent,
+            bounds: {target: mainContent, minX: 0, maxX: 0, top: 0,left: 0, width: 0, height: 0},
+            type: "x",
+            throwProps: true,
+            allowNativeTouchScrolling: true,
+            zIndexBoost: true,
+
+            onDragStart: (e) => {
+              if (e.touches) e.clientX = e.touches[0].clientX;
+              xPos = Math.round(e.clientX);
+              gsap.set('.ring', {cursor:'grabbing'})
+            },
+
+            onDrag: (e) => {
+              if (e.touches) e.clientX = e.touches[0].clientX;
+              gsap.to('.ring', {
+                rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
+              });
+              console.log(xPos, e.clientX)
+              xPos = Math.round(e.clientX);
+            },
+
+            onDragEnd: (e) => {
+              gsap.set('.ring', {cursor:'grab'});
+            }
+          })
+
+          function getRotate(i) {
+            return i * -36;
+          }
+
         })
-
-      function getBgPos(i) { // returns the background-position string to create parallax movement in each image
-        return (100 - gsap.utils.wrap(0, 360, gsap.getProperty('.ring', 'rotationY') - 180 - i * 36) / 360 * 500) + 'px 0px';
-      }
-
-      function getRotate(i) { // returns the background-position string to create parallax movement in each image
-        console.log(i * -36)
-        return i * -36;
-      }
+      })
     }
   },
 }
@@ -63,7 +117,7 @@ export default {
 <style>
 .stage,
 .ring,
-.carde {
+.card-item {
   width: 100%;
   height: 100%;
   transform-style: preserve-3d;
@@ -75,7 +129,7 @@ export default {
 }
 
 .ring,
-.carde,
+.card-item,
 .wrapper {
   position: absolute;
 }
@@ -87,5 +141,13 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+
+.card-item {
+  flex-shrink: 0;
+  width: 300px;
+  height: 300px;
+  margin-left: 20px;
+  background-color: white;
 }
 </style>
